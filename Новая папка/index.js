0c9fa7747458c10a21a7e44b7fcc9ecc2e81b528
Satsura-code <saishona.satsura@gmail.com>
2024-07-05 12:57:44 +0500

    Update index.js

--- Новая папка/index.js
+++ Новая папка/index.js
@@ -1,2 +1,18 @@
-if a > b 
-alert('Самый большой' + a); 
+//Data types
+//number, string, boolean, null, undefined, object, bigInt, symbol
+
+let age = 17; //number
+const name = "Ozod"; //string
+let HaveMillion$ = false; //boolean : true or false
+let trash = null; //null
+let box; //undefined
+let shop = {
+    title: 'Korzinka',
+    discription: 'Input',
+    shopcount: 120,
+} //object
+let BigInt = 13241323214543213124325n; //bigInt
+
+let symbolEx1 = Symbol('name')
+let symbolEx2 = Symbol('name')
+console.log(symbolsEx1 === symbolEx2); //FALSE
